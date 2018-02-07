b820a663ed5055516a2ba401fa28415c0fde12a7
SaKanya <36222092+SaKanya@users.noreply.github.com>
2018-02-07 14:28:32 +0700

    Add files via upload

--- mybot.php
+++ mybot.php
@@ -1,5 +1,6 @@
 ﻿<?php
-$access_token = 'hOKDUKtWqywjF8u0K4NdBLGcfTxb97BBlalAp1I0PCZ1M6+7U40Mmmpqc8f5EcjNLzdEU2A7W/G0o2vbxoLffjITIUJUv/CL6JP6aYsYtdra/azB9PpjEU++HSwFu/CoTBkhxH/VTZBYpWzu34gITwdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ZOUhDwNeYGrUPLk8tvle0s+oSqb1mXW3lxoWno8KDI+icOuObCLY9NpzX/pOS5MaT7VmKMtFpRQBr91Htz+O4yDxTNZzygCePRkf1XIrAvd1E/9DUbSZ/FrBgLNdE9mRCTggMAFkfW+jNaGUTVZCrQdB04t89/1O/w1cDnyilFU=
+';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
